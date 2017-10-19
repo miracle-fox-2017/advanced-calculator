09abea8f6f9ea8354de547d2d42da00951912113
vbagustinus <vbagustinus@gmail.com>
2017-10-19 11:47:46 +0700

    advance calculator

--- calculator.js
+++ calculator.js
@@ -3,18 +3,39 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.hasil = 0
+    this.pi = 3.14
   }
-  add () {
+  add (input) {
+     this.hasil += input
+     return this
   }
-  substract () {
+  substract (input) {
+    this.hasil -= input
+    return this
   }
-  multiply () {
+  multiply (input) {
+    this.hasil *= input
+    return this
   }
-  divide () {
+  divide (input) {
+    this.hasil /= input
+    return this
   }
-  square () {
+  square (input) {
+    this.hasil = Math.pow(this.hasil, input)
+    // console.log('-----setelah itu', this.hasil)
+    return this
   }
   squareRoot () {
+    this.hasil = Math.sqrt(this.hasil)
+    // console.log('harusnyaAkarpangkat',Math.sqrt(this.hasil));
+    return this
+  }
+  cirle(input){
+    //pi r2
+    this.hasil = this.pi * (Math.pow(input, 2))
+    return this
   }
 }
 
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict'
 
 const {Calculator} = require("./calculator.js")
+const calculator =  new Calculator()
+const calculatorSquare =  new Calculator()
 
 // execute function on calculator.js in here
+console.log('awal', calculator.add(2).multiply(9).divide(2).hasil)
+console.log('pangkat', calculatorSquare.add(2).multiply(9).divide(2).square(2).hasil);
