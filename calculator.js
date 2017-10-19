91c989ba111b62cc77f443c45631e21f96196655
ipunganwar <ipung.anwar1@gmail.com>
2017-10-19 20:38:31 +0700

    release 0

--- calculator.js
+++ calculator.js
@@ -3,18 +3,42 @@
 class Calculator {
   //write your code here
   constructor () {
+    this.input = 0
+    this.pi = 3.14
   }
-  add () {
+  add (num1, num2) {
+    this.input = num1 + num2 
+    return this
   }
-  substract () {
+  substract (sub) {
+    this.input -= sub
+    return this
   }
-  multiply () {
+  multiply (mul) {
+    this.input *= mul
+    return this
   }
-  divide () {
+  divide (div) {
+    this.input /= div
+    return this
   }
-  square () {
+  square (sqr) {
+    this.input = Math.pow(this.input, sqr)
+    return this
   }
   squareRoot () {
+    this.input = Math.sqrt(this.input)
+    return this
+  }
+
+  luasLingkaran(jari) {
+    this.input = pi
+    this.square(2).multiply(this.pi)
+    return this
+  }
+
+  print() {
+    return `Hasil perhitungan = ${this.input}`
   }
 }
 
@@ -25,6 +49,11 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+let calc = new Calculator()
+console.log(calc.add(3,3).substract(4).multiply(2).divide(2).square(4).print())
+console.log(calc.add(3,12).substract(4).multiply(2).square(4).print())
+
 
 module.exports = {
   Calculator
