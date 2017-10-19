cc068b75e5160d4f0eef5a2811ee993d862d489c
Septian Ahmad Fujianto <septianahmad@naisinpo.com>
2017-10-19 11:02:00 +0700

    Advance calculator

--- calculator.js
+++ calculator.js
@@ -2,19 +2,42 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (number) {
+    this.number = number;
   }
-  add () {
+
+  add (num) {
+    this.number += num;
+    return this;
   }
-  substract () {
+
+  substract (num) {
+    this.number -= num;
+    return this;
   }
-  multiply () {
+
+  multiply (num) {
+    this.number = this.number * num;
+    return this;
   }
-  divide () {
+
+  divide (num) {
+    this.number = this.number / num;
+    return this;
   }
-  square () {
+
+  square (num) {
+    this.number = Math.pow(this.number, num);
+    return this;
   }
-  squareRoot () {
+
+  squareRoot (num) {
+    this.number = Math.sqrt(num);
+    return this;
+  }
+
+  static pi() {
+    return Math.PI;
   }
 }
 
@@ -25,6 +48,10 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+
+let countNumber = new Calculator(5);
+console.log(countNumber.add(4).substract(3).multiply(2).divide(3).square(3).squareRoot(4).number);
+console.log(Calculator.pi())
 
 module.exports = {
   Calculator
