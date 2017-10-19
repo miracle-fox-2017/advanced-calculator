727ae24bc5116641b42db49fc3c84cf06049ce28
github_d18mm <ilham18mm@gmail.com>
2017-10-19 22:39:30 +0700

    ilham calculator advanced

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 'use strict'
 
 const {Calculator} = require("./calculator.js")
+var kalkulator = new Calculator(9);
+console.log(kalkulator.add(2))
+console.log(kalkulator.substract(2))
+console.log(kalkulator.multiply(2))
+console.log(kalkulator.squareRoot());
+console.log(kalkulator.constaPi(9));
 
 // execute function on calculator.js in here
