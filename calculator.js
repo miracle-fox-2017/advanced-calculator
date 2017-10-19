1f3b88877f0f819706e16f8f1135b3f8a9950e97
Ahmad <matt.syahab@gmail.com>
2017-10-19 11:53:51 +0700

    first Commit Calculator done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,49 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (num) {
+    this.num = num;
+    this.Pi = 3.14;
+    // wideCircle untuk luas lingkaran
+    this.wideCircle = null
   }
-  add () {
+  add(addNum) {
+    this.addNum = addNum;
+    this.num = this.num + this.addNum;
+    return this
   }
-  substract () {
+  substract (subNum) {
+    this.subNum = subNum;
+    this.num -= this.subNum
+    return this
   }
-  multiply () {
+  multiply (mulNum) {
+    this.mulNum = mulNum;
+    this.num *= this.mulNum
+    return this
   }
-  divide () {
+  divide (divNum) {
+    this.divNum = divNum
+    this.num /= this.divNum
+    return this
   }
-  square () {
+  square(power) {
+    this.power = power
+    this.num = Math.pow(this.num, this.power)
+    return this
   }
-  squareRoot () {
+  squareRoot (radius) {
+    this.radius = radius
+    this.wideCircle = this.Pi * Math.pow(this.radius, 2)
+    return this
+  }
+
+  print (){
+    console.log(`current number = ${this.num}`);
+    //jika ada luas  lingkaran maka output akan keluar
+    if (this.wideCircle){
+      console.log(`current wide Circle ${this.wideCircle}`);    
+    }
   }
 }
 
@@ -25,6 +55,9 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+var calculate = new Calculator(10);
+calculate.print()
+console.log(calculate)
 
 module.exports = {
   Calculator
